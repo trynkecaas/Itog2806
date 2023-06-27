4f117688ce51c7576189333d27eb1ee3fa9f8bdb
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-06-28 00:45:57 +0300

    itog100

--- itog/Program.cs
+++ itog/Program.cs
@@ -1,6 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите количество элементов массива: ");
-int m = Convert.ToInt32(Console.ReadLine);
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [m];
 void array (string[] stringArray)
 {
@@ -15,26 +14,27 @@ string [] symbol(string [] stringArray)
   int n = 0;
   for (int i = 0; i<stringArray.Length;i++)
   {
-    if(stringArray[i].Length<=3)
+    if(stringArray[i].Length <=3)
     n++;
    }
-} 
 string [] rez = new string [n];
 int j = 0;
 for ( int i=0; i< stringArray.Length; i++)
 {
-  if ( stringArray[i].Length;i++)
+  if ( stringArray[i].Length<=3)
   { 
     rez[j]= stringArray[i];
     j++;
   }
+  }
+  return rez;
 }
 void printA(string [] stringArray)
 {
   Console.Write("[");
   for (int i=0; i<stringArray.Length;i++)
   {
-    Console.WriteLine($"{stringArray[i]}', ");
+    Console.Write($"{stringArray[i]}', ");
   }
   Console.Write("]");
 }
