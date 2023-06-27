390d469314fe37f55235399b7b53d112ce04efee
trynkecaas <122730663+trynkecaas@users.noreply.github.com>
2023-06-28 00:00:57 +0300

    itog

---
+++ itog/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine);
+string [] stringArray = new string [m];
+void array (string[] stringArray)
+{
+  for (int i = 0; i<stringArrayLength;i++)
+  {
+    Console.WriteLine($"Введите {i+1} элемент массива");
+    stringArray[i] =Console.ReadLine();  
+  }
+}
